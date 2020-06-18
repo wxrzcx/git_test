903d9bd2befe01d7a524935744af3f2fcc9c80f2
wxr <1823296352@qq.com>
2020-06-18 19:11:49 +0800

    第二次多人协作 更改usercontroller

--- src/main/java/com/offcn/controller/UserController.java
+++ src/main/java/com/offcn/controller/UserController.java
@@ -3,6 +3,6 @@ package com.offcn.controller;
 public class UserController {
 
     public static void main(String[] args) {
-        System.out.println("这是左边的");
+        System.out.println("这是右边的");
     }
 }
